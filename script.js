6e98da6a0b56763fc3295a5c7093916f30fa7be7
Isaac <isaac.keninger@gmail.com>
2025-10-21 11:07:01 -0500

    Add Comments

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Script for in-game mechanics and screen updates
 
-let cells = document.querySelectorAll('.cell')
-let turn = 0;
-let winCondition = false;
-let moveCount = 0;
-let winner = "";
+let cells = document.querySelectorAll('.cell') // Selects all the HTML Div's named cell
+let turn = 0; // Turn of Game
+let winCondition = false; // True if game is won, false otherwise
+let moveCount = 0; // Tracks number of game turns/moves
+let winner = ""; // Tracks Winner of the game (if applicable)
+
+// This tracks which value is inside each cell of the board.
 let gameTracker = new Map([
     ["1", null],
     ["2", null],
@@ -17,6 +19,7 @@ let gameTracker = new Map([
     ["9", null],
 ]);
 
+// All possible pattens to win the game
 const winningVals = [
     [1,2,3],
     [4,5,6],
@@ -28,12 +31,13 @@ const winningVals = [
     [3,5,7]
 ];
 
-cells.forEach((cell) => {
-    cell.addEventListener('click', function(){
+// 
+cells.forEach((cell) => { // This will iterate over all cells to give properties
+    cell.addEventListener('click', function(){ // Activates below code if a cell is clicked
         if (winCondition) return; // checks win condition
         if (cell.innerText != '') return; // ignore clicks on filled cells
 
-        // Turns
+        // Set Value in cell based off Turn/Player
         if (turn == 0){
             cell.innerText = 'O';
             gameTracker.set(cell.id, 'O');
@@ -44,14 +48,15 @@ cells.forEach((cell) => {
 
         moveCount++; // Update Count of Turns
 
-        let winner = winCheck();
+        let winner = winCheck(); //Returns a string to be checked
         if (winner) {
             winCondition = true;
             showEnd(winner);
             return;
         }
 
-        if (moveCount == 9){
+        // Check Stalemate Condition
+        if (moveCount == 9){ 
             winCondition = true;
             showEnd("Stalemate")
             return;
@@ -62,11 +67,11 @@ cells.forEach((cell) => {
     });
 });
 
-reset.addEventListener('click', resetGame);
+reset.addEventListener('click', resetGame); //Reset Game if Button Clicked
 
 // Functions
 function winCheck() {
-    for (let sequence of winningVals) {
+    for (let sequence of winningVals) { //for all sequences
         // Check if all positions are 'O'
         if (sequence.every(pos => gameTracker.get(pos.toString()) == 'O')) {
             return "Player 1";
@@ -79,6 +84,7 @@ function winCheck() {
     return null;
 }
 
+// This changes the screen depending on who winner is / stalemate.
 function showEnd(winner){
 
     if (winner != "Stalemate"){
@@ -90,6 +96,7 @@ function showEnd(winner){
     document.getElementById("reset").style.display = "block";
 };
 
+// This resets game to inital board.
 function resetGame(){
     cells.forEach(cell => {
         cell.innerText = '';
